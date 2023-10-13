074a9d09da91c97c9d9fdc2b3628d5130859a2d9
Douglas Carmona <douglas.albertocg@gmail.com>
2023-10-13 16:18:53 -0300

    change: update mars screw data

--- R/mars_screw.R
+++ R/mars_screw.R
@@ -14,24 +14,5 @@
 #'  \item screw_index: the index corresponding to the screw observation (screw_index=4180)
 #'}
 #'
-#'@examples
-#'
-#' # upload matrix 
-#' A <- mars_screw$SI_matrix;
-#' B <- mars_screw$geographic_matrix
-#' screw_index <- mars_screw$screw_index
-#' ## looking for the brightest cells
-#' maximun_at_each_cell<- apply(A[ ,1:64], 1, max)
-#' ten_brightest_cells <-order(maximun_at_each_cell, decreasing=TRUE)[1:10]
-#'
-#' ## plot locations where the ten brightest cells are.
-#' plot(B, pch=19 )
-#' points(B[ten_brightest_cells, ], pch=19,col="yellow" )
-#'
-#' ## plot locations where the screw are.
-#' points(B[screw_index, ], pch=19,col="blue" )
-
-#'
 #' @source \url{https://www.nasa.gov/mission_pages/msl/multimedia/pia16225.html}
 "mars_screw"
-
